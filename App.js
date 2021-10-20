7f52e5b17f649fee61e29b8660cd27625e995536
andikscript <andikscript@gmail.com>
2021-10-20 20:54:58 +0700

    update file App.js

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React, {Component, useEffect, useLayoutEffect, useState} from 'react';
-import {ActivityIndicator, View} from 'react-native';
+import {View} from 'react-native';
 
-const App = () => {
+import {Atas, Tengah, Bawah} from './components/dnaindicator/ViewBox';
+
+const ActivityIndicatorDna = () => {
+  const [condition, setCondition] = useState(false);
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setCondition(!condition);
+    }, 1800);
+  }, [condition]);
+
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "white",
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Container condition={condition} size={50} />
     </View>
-  )
-}
+  );
+};
+
+const Container = ({condition, size}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size * 2 + 5,
+      }}>
+      <Atas condition={condition} paddingLeft={size / 10} />
+      <Tengah condition={condition} />
+      <Bawah condition={condition} paddingLeft={size / 10} />
+    </View>
+  );
+};
+
+export default ActivityIndicatorDna;
